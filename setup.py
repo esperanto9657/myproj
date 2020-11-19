71d9c8069c4e457d092a366e9a52a8cceaef9cf4
esperanto9657 <esperanto9657@live.jp>
2020-11-20 03:06:00 +0900

    Create package system

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+ 
+setup(
+    name='abc181d',    #パッケージ名
+    version="0.0.1",
+    author='Kaki Shu',
+    classifiers=[
+        "Development Status :: 1 - Planning"
+    ]
+)
